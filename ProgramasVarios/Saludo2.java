c06b3c2b59a0cd46ae2f5cf6826aba1ebd3bd836
luis8217 <foknite2.0@gmail.com>
2025-10-23 11:14:03 -0500

    4to comentario

--- ProgramasVarios/Saludo2.java
+++ ProgramasVarios/Saludo2.java
@@ -1,9 +1,7 @@
+import java.util.Scanner;
 public class Saludo2{
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-        
-			
-		
+		Scanner sc =new Scanner(System.in);
 		String nombre;
 		System.out.println("Dime tu nombre:");
 		nombre=sc.nextLine();
